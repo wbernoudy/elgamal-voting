9ba04dc4766a357d21e227a9d8222cd97ed2fbdf
William Bernoudy <william@williambernoudy.com>
2016-04-03 13:44:23 +0800

    Finished all parts for basic voting

--- pedersen.py
+++ pedersen.py
@@ -1,4 +1,5 @@
 from functools import reduce
+from operator import and_
 import random
 
 def prod(iterable):
@@ -30,28 +31,6 @@ def eval_poly(poly, x, q):
     return y
 
 
-def lagrange(x, y, G):
-    assert len(x) == len(y) and len(x) > 0, "Lengths of x and y must equal and non-zero."
-    x_len = len(x)
-    f = [0] * x_len
-    for i in range(x_len):
-        partial = []
-        combo_list = list(x)
-        combo_list.pop(i)
-        for j in range(x_len):
-            c = 0
-            for k in combinations(combo_list, j):
-                c += prod(map(lambda q: -q, k), G)
-            partial.append(c)
-        d = 1
-        for j in range(x_len):
-            if j != i:
-                d = (d * (x[i] - x[j]))
-
-        partial = map(lambda q: div(mul(q, y[i]),  d), partial) # fix this babe
-        f = [(m + n) % G for m, n in zip(f, partial)] # also needs % G
-
-
 def elgamal_encrypt(message, public_key, g, q):
     m = pow(g, message, p) # TODO: pretty sure about this p (and the next few) but not totally sure
     r = randint(q)
@@ -60,6 +39,30 @@ def elgamal_encrypt(message, public_key, g, q):
     return (x, y)
 
 
+def beacon(p_id, vals, q):
+    #r = randint(q)
+    r = 23423452356 # TODO: make this a real thing
+    return r
+
+## The following would be on the server, represents the public bulletin board ##
+public_key_shares = {}
+def publish_public_key_share(p_id, share):
+    public_key_shares[p_id] = share
+
+def get_public_key_shares():
+    return public_key_shares
+
+
+pedersen_commits = {}
+def publish_pedersen_commit(p_id, commit):
+    assert len(commit) == 5, "Pedersen commit should be 5 numbers."
+    pedersen_commits[p_id] = (commit)
+
+def get_pedersen_commit(p_id):
+    if p_id in pedersen_commits:
+        return pedersen_commits[p_id]
+    else:
+        return None
 
 
 class Pedersen:
@@ -70,38 +73,27 @@ class Pedersen:
         self.g = g
         self.n = n
         self.party_id = party_id
-        self.global_shares = {}
-        self.public_key_shares = {}
+        #self.public_key_shares = {}
         self.proofs = {}
-        self.c_values = {}
-        self.r_values = {}
+        self.pedersen_commits_verified = {p_id: False for p_id in range(n) if p_id != self.party_id}
+        self.global_decrypt_shares = {}
 
-        self.poly = gen_poly(n, q)
-        self.secret = self.poly[0] # poly(0) is our secret part of the public key
+        #self.poly = gen_poly(n, q)
+        self.secret = randint(q) #TODO: make sure this is good#self.poly[0] # poly(0) is our secret part of the public key
         self.public_key_share = pow(g, self.secret, p) # TODO: make sure this is p
+        publish_public_key_share(party_id, self.public_key_share)
 
-        self.shares = {} # format is {p_id: (share, commit)}
-        for p_id in range(n):
-            if p_id != self.party_id: # we don't publish our own share
-                share = eval_poly(self.poly, p_id+1, q)
-                commit = pow(g, share, p) # g^share mod p is the verifiable commit
-                self.shares[p_id] = (share, commit)
-        
+        #self.shares = {} # format is {p_id: (share, commit)}
+        #for p_id in range(n):
+        #    if p_id != self.party_id: # we don't publish our own share
+        #        share = eval_poly(self.poly, p_id+1, q)
+        #        commit = pow(g, share, p) # g^share mod p is the verifiable commit
+        #        self.shares[p_id] = (share, commit)
 
-    def publish_shares(self):
-        return self.shares
+    #def receive_public_key_share(self, p_id, share):
+    #    self.public_key_shares[p_id] = share
 
-    def publish_public_key_share(self):
-        return self.public_key_share
-
-    def receive_share(self, p_id, share):
-        """In this case p_id is the ID of the party we are receiving from"""
-        self.global_shares[p_id] = share
-
-    def receive_public_key_share(self, p_id, share):
-        self.public_key_shares[p_id] = share
-
-    def sum_shares(self):
+    """def sum_shares(self):
         assert len(self.global_shares) == self.n-1, "Have not received all shares."
         assert len(self.public_key_shares) == self.n-1, "Have not received all shares."
         self.global_share = sum(x[0] for x in self.global_shares.values())
@@ -109,130 +101,126 @@ class Pedersen:
         # We now have every commit
         m = prod(self.public_key_shares.values())
         self.public_key = (m * self.public_key_share) % self.p
+        """
 
+    def make_public_key(self):
+        public_key_shares = get_public_key_shares() # from the bulletin board
+        if len(public_key_shares) != self.n:
+            print("Not everyone has publish their share of the public key yet.")
+            return
+
+        assert public_key_shares[self.party_id] == self.public_key_share, "Someone changed our share on the bulletin board!"
+        
+        self.public_key_shares = public_key_shares
+        self.public_key = prod(public_key_shares.values())
     
-    def log_ZKP_prove_step1(self, ciphertext):
+    def log_ZKP_prove(self, ciphertext):
         self.ciphertext = ciphertext
-        x = ciphertext[0]
-        y = ciphertext[1]
+        #x = ciphertext[0]
+        #y = ciphertext[1]
+
+        g = self.g 
+        p = self.p
+        q = self.q
+
 
         # w is our share of the decrypted ciphertext
-        w = pow(x, self.secret, p) # TODO: is this mod p or q?
-        self.w = w # we need w for decription later and for step 2
+        decrypt_share = pow(ciphertext[0], self.secret, p)
+        self.decrypt_share = decrypt_share # we need w for decription later and for step 2
+        h = ciphertext[0]
+        x = self.public_key_share
+        y = decrypt_share # the name in the protocol in figure 2
+        alpha = self.secret # TODO: double check
+
 
         # We now need to prove that log_g(self.public_key_share) == log_x(w)
         # or, in other words, we haven't changed our secret when calculating w
         # This is done using the zero knowledge proof from Pedersen
-        r = randint(self.q)
-        self.random_w = r
-        a = pow(self.g, r, self.p) # TODO: are these mod q?
-        b = pow(x, r, self.p)
-        self.a = a # these are unnecessary to keep
-        self.b = b
+        w = randint(q)
+        a = pow(g, w, p)
+        b = pow(h, w, p)
 
-        return [w, a, b] # now anyone should be able to verify that w is #legit
+        commit0 = (y, w, a, b)
+        c = beacon(self.party_id, commit0, q)
 
-    def log_ZKP_verify_step2_receive(self, p_id, proof):
-        proof.append(randint(self.q)) # the c value
-        self.proofs[p_id] = proof
+        r = (w + alpha*c) % q # TODO: pretty sure about this q, maybe double check it
 
-    def log_ZKP_verify_step2_send(self):
-        assert len(self.proofs) == self.n-1, "Haven't received all proofs yet."
-        tests = {p_id: proof[3] for p_id, proof in self.proofs.items()}
-        return tests
+        commit = commit0 + (r,)
 
-    def log_ZKP_prove_step3_receive(self, p_id, c):
-        self.c_values[p_id] = c
+        publish_pedersen_commit(self.party_id, commit) # now anyone should be able to verify that w is #legit
 
-    def log_ZKP_prove_step3_send(self):
-        assert len(self.c_values) == self.n-1, "Haven't received all c values yet."
-        print("c", c)
-        r = {p_id: (self.random_w + c*self.secret) for p_id, c in self.c_values.items()} # TODO: mod q????
-        self.r = r
-        return r
+    def log_ZKP_verify(self, p_id):
+        commit = get_pedersen_commit(p_id)
+        if commit is None:
+            print("Player has not published Pedersen commit yet.")
+            return
 
-    def log_ZKP_verify_step4_receive(self, p_id, r):
-        self.r_values[p_id] = r
+        g = self.g
+        p = self.p
+        q = self.q
+        h = self.ciphertext[0]
 
-    def log_ZKP_verify_step4_verify(self):
-        assert len(self.r_values) == self.n-1, "Haven't received all r values yet."
+        x = self.public_key_shares[p_id]
+        (y, w, a, b, r) = commit
+        c = beacon(p_id, (x, y, w, a, b), q)
 
-        abort = False
+        test1 = (pow(g, r, p) == (a*pow(x, c, p)) % p)
+        test2 = (pow(h, r, p) == (b*pow(y, c, p)) % p)
+
+        if not (test1 and test2):
+            print(test1, test2)
+            print(self.party_id, "could not verify", p_id)
+        else:
+            self.pedersen_commits_verified[p_id] = True
+            self.global_decrypt_shares[p_id] = y
+
+    def log_ZKP_verify_all(self):
         for p_id in range(self.n):
             if p_id != self.party_id:
-                r = self.r_values[p_id]
-                a = self.proofs[p_id][1]
-                x = self.public_key_shares[p_id]
-                c = self.proofs[p_id][3] #self.c_values[p_id]
-                h = self.ciphertext[0]
-                b = self.proofs[p_id][2]
-                y = self.proofs[p_id][0]
-
-                test1 = (pow(self.g, r, self.p) == (a*pow(x, c, self.p)) % self.p)
-                test2 = (pow(h, r, self.p) == (b*pow(y, c, self.p)) % self.p)
-
-                if not (test1 and test2):
-                    print(test1, test2)
-                    abort = True
-                    print(self.party_id, "Uh oh!", p_id, "failed their ZKP. Abort!!!!")
-                else:
-                    print("All tests passed!")
-
-        self.abort = abort
+                self.log_ZKP_verify(p_id)
 
     def decrypt(self):
-        assert not self.abort, "ZKPs were not completed successfully. Decrypt is dangerous."
-        #print(self.proofs)
+        all_verified = reduce(and_, self.pedersen_commits_verified.values())
+        if not all_verified:
+            print("Haven't yet verified all other players. Aborting decryption.")
+            return
 
-        w = map(lambda x: x[0], self.proofs.values())
-        P = prod(w) * self.w
+        P = (prod(self.global_decrypt_shares.values()) * self.decrypt_share) % self.p
         message = mod_div(self.ciphertext[1], P, self.p) # TODO: check q
 
         return message
 
 
-commits = {}
-def publish_vote(p_id, step, commit):
-    if step == 0:
-        commits[p_id] = [commit]
-    else:
-        assert p_id in commits, "Voter is trying to commit second step before step 1."
-        assert len(commits[p_id]) == 2, "Voter didn't use a published c value."
+## The following would be on the server, represents the public bulletin board ##
+voter_commits = {}
+def publish_vote(p_id, commit):
+    assert len(commit) == 10, "Commit should consist of 10 numbers."
+    voter_commits[p_id] = (commit)
 
-        commits[p_id].append(commit)
-
-def beacon(p_id, q):
-    assert len(commits[p_id]) == 1, "Voter hasn't published commit0 yet."
-    r = randint(q)
-    commits[p_id].append(r)
-    return r
-
-def get_commit(p_id):
-    if len(commits[p_id]) == 3:
-        return commits[p_id]
+def get_voter_commit(p_id):
+    if p_id in voter_commits:
+        return voter_commits[p_id]
     else:
         return None
-    
-
-
-
 
 class Voter(Pedersen):
     def __init__(self, p, g, n, voter_id, candidates = 2):
         super(Voter, self).__init__(p, g, n, voter_id)
         self.voter_id = voter_id
         self.candidates = candidates
+        self.votes_verified = {p_id: False for p_id in range(n) if p_id != voter_id}
+        self.global_votes = {}
         
         # these are defined such that the final decrypted value will be g^x
         # where x is the total # of yes's - no's
         self.yes = self.g
         self.no = pow(self.g, p-2, p) # multiplicative inverse of g, g^-1
 
-    def get_vote(self, vote):
+    def set_vote(self, vote):
         assert 0 <= vote < self.candidates, "Not a valid vote."
         self.vote = vote
     
-    def encrypt_vote(self):
+    def encrypt_and_prove(self):
         #v = []
 
         # temporary, simplified method
@@ -242,21 +230,18 @@ class Voter(Pedersen):
 
         self.v = v
 
-        encrypted_vote = elgamal_encrypt(v, self.public_key, self.g, self.q)
-        self.encrypted_vote = encrypted_vote
-        return encrypted_vote
-
-    def prove(self):
         (alpha, w, r1, d1) = (randint(self.q) for x in range(4))
 
         # these are just for making it cleaner
         p = self.p
         q = self.q
         h = self.public_key
-        v = self.v
+        #v = self.v
 
         x = pow(g, alpha, p)
         y = (pow(h, alpha, p)*v) % p
+        #(x, y) = self.encrypted_vote
+        self.encrypted_vote = (x, y) # this is our own encrypted vote
         a1 = (pow(g, r1, p)*pow(x, d1, p)) % p
         b1 = (pow(h, r1, p)*pow((y*v)%p, d1, p)) % p
         a2 = pow(g, w, p)
@@ -265,25 +250,28 @@ class Voter(Pedersen):
             commit0 = (x, y, a1, b1, a2, b2)
         else:
             commit0 = (x, y, a2, b2, a1, b1)
-
-        publish_vote(self.party_id, 0, commit0) # 0 is the first step
         # done with the first part
 
-        # this represents getting the random value from the verifier
-        c = beacon(self.party_id, self.q)
+        # This represents getting the random value from the verifier.
+        # To keep the protocol non-interactive, this will just be a hash
+        # that the prover can't control. This also solves the problem of
+        # the malicious verifier.
+        c = beacon(self.voter_id, commit0, q)
 
-        d2 = (c - d1) % q # TODO: figure out how to deal with negatives
+        d2 = (c - d1) % q
         r2 = (w - alpha*d2) % q
         if self.vote:
             commit1 = (d1, d2, r1, r2)
         else:
             commit1 = (d2, d1, r2, r1)
 
-        publish_vote(self.party_id, 1, commit1)
+        commit = commit0 + commit1
+
+        publish_vote(self.voter_id, commit)
         # done with the protocol
 
     def verify_vote(self, p_id):
-        commit = get_commit(p_id)
+        commit = get_voter_commit(p_id)
         if commit is None:
             print("Voter hasn't published full proof yet.")
             return
@@ -294,9 +282,8 @@ class Voter(Pedersen):
         g = self.g
         h = self.public_key
 
-        (x, y, a1, b1, a2, b2) = commit[0]
-        c = commit[1]
-        (d1, d2, r1, r2) = commit[2]
+        (x, y, a1, b1, a2, b2, d1, d2, r1, r2) = commit
+        c = beacon(p_id, commit[:6], q) # the c value should be the hash of the first 6 values commited
 
         test1 = (c == (d1 + d2) % q) # TODO: is mod p necessary?
         test2 = (a1 == (pow(g, r1, p)*pow(x, d1, p)) % p)
@@ -308,90 +295,79 @@ class Voter(Pedersen):
             print(test1, test2, test3, test4, test5)
             print(p_id, "failed their verification! Someone's cheating! Abort!!")
         else:
-            print(self.party_id, "passed", p_id, "on voting verification.")
+            print(self.voter_id, "passed", p_id, "on voting verification.")
+            self.votes_verified[p_id] = True
+            self.global_votes[p_id] = (x, y)
+
+    def verify_vote_all(self):
+        for v_id in range(self.n):
+            if v_id != self.voter_id:
+                self.verify_vote(v_id)
+
+    def calc_vote_step1(self):
+        all_verified = reduce(and_, self.votes_verified.values())
+        if not all_verified:
+            print("Haven't yet verified all other voters. Aborting vote calculation.")
+            return
+
+        # include our own vote
+        self.global_votes[self.voter_id] = self.encrypted_vote
+
+        # this step does the additive homomorphic encryption
+        # it gives us (x_0*x_1*...*x_n, y_0*y_1*...y_n) for all w_i = (x_i, y_i)
+        w = reduce(lambda x,y: (x[0]*y[0], x[1]*y[1]), self.global_votes.values())
+        w = (w[0]%p, w[1]%p) # make it mod p
+
+        # w is now the encrypted value holding the result of the vote.
+        # We now want to decrypt it as a group, first step is publishing our
+        # ZKP commit
+        self.log_ZKP_prove(w)
+
+    def calc_vote_step2(self):
+        self.log_ZKP_verify_all()
+        # We should now be able to decrypt the value if all verifications are good
+
+        out = self.decrypt()
+        self.out = out
+        return out
+
+
+
 
         
         
 
 
 
-"""Testing Pederson public generation, zero knowledge proofs, and decryption"""
+#Testing Pederson public generation, zero knowledge proofs, and decryption
 
-num_players = 3
-players = [Voter(p, g, num_players, x) for x in range(num_players)]
+num_voters = 3
+votes = [0, 0, 0]
+voters = [Voter(p, g, num_voters, x) for x in range(num_voters)]
 
-for player in players:
-    s = player.publish_shares()
-    for p_id,share in s.items():
-        players[p_id].receive_share(player.party_id, share)
-        
+for vt in voters:
+    vt.make_public_key()
 
-for p_id in range(num_players):
-    pk_share = players[p_id].publish_public_key_share()
-    for p_id2 in range(num_players):
-        if p_id2 != p_id:
-            players[p_id2].receive_public_key_share(p_id, pk_share)
+for vt in voters:
+    vt.set_vote(votes[vt.voter_id])
 
-for player in players:
-    player.sum_shares()
+for vt in voters:
+    vt.encrypt_and_prove()
 
-pk = players[0].public_key
-for player in players:
-    assert pk == player.public_key, "Public key mismatch"
+for vt in voters:
+    vt.verify_vote_all()
+    
+for vt in voters:
+    vt.calc_vote_step1()
 
-s = sum(pl.secret for pl in players)
-assert pow(g, s, p) == pk, "Invalid elgamal keypair."
+outs = []
+for vt in voters:
+    outs.append(vt.calc_vote_step2())
 
-my_message = 23466434
-ciphertext = elgamal_encrypt(my_message, pk, g, q)
+out = outs[0]
+for o in outs:
+    assert o == out, "Decryption resulted in different values"
 
-for player in players:
-    proof = player.log_ZKP_prove_step1(ciphertext)
-    for p_id in range(num_players):
-        if p_id != player.party_id:
-            players[p_id].log_ZKP_verify_step2_receive(player.party_id, proof)
-
-for player in players:
-    sent = player.log_ZKP_verify_step2_send()
-    for p_id,c in sent.items():
-        players[p_id].log_ZKP_prove_step3_receive(player.party_id, c)
-
-for player in players:
-    sent = player.log_ZKP_prove_step3_send()
-    for p_id,r in sent.items():
-        players[p_id].log_ZKP_verify_step4_receive(player.party_id, r)
-
-print("final c", c)
-thingy = pow(g, my_message, p)
-for player in players:
-    player.log_ZKP_verify_step4_verify()
-    assert thingy == player.decrypt()
+assert out == mod_div(1, g**3, p), "Bad out"
 
 
-
-
-
-
-# testing the voting
-v0 = players[0]
-v1 = players[1]
-v2 = players[2]
-
-v0.get_vote(0)
-v1.get_vote(1)
-v2.get_vote(0)
-
-v0.encrypt_vote()
-v1.encrypt_vote()
-v2.encrypt_vote()
-
-v0.prove()
-v1.prove()
-v2.prove()
-
-v0.verify_vote(1)
-v0.verify_vote(2)
-v1.verify_vote(0)
-v1.verify_vote(2)
-v2.verify_vote(0)
-v2.verify_vote(1)
